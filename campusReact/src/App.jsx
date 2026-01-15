82b0b8b3c255374d84cbfa9a959501cc69dbe808
Your Name <148898409+bedxnn@users.noreply.github.com>
2026-01-14 20:17:05 -0500

    auth context

--- campusReact/src/App.jsx
+++ campusReact/src/App.jsx
@@ -1,4 +1,5 @@
 import Signup from "./auth/Signup"
+import Login from "./auth/Login"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Signup/>
+      <Login/>
     </>
   )
 }
